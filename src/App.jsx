20392a9832d3af33e94317a6f9dd550098c17020
gus-p3 <sinnamexd3432@gmail.com>
2026-01-15 16:37:02 -0600

    Fix: Regreso a su estado del segundo ejercicio de la clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
 import React from "react";
-import Navbar from "./componentes/Navbar";
-import Banner from "./componentes/Banner";
+import Saludo from "./componentes/Saludo";
+import Listado from "./componentes/Listado";
 
 const App = () => {
+  console.log("Componente saludo");
   return (
     <>
-      <div className="overflow-x-hidden">
-        <Navbar />
-      </div>
-      <Banner />
+      <Listado />
     </>
   );
 };
 
 export default App;
-
-// import React from "react";
-// import Saludo from "./componentes/Saludo";
-// import Listado from "./componentes/Listado";
-
-// const App = () => {
-//   console.log("Componente saludo");
-//   return (
-//     <>
-//       <Listado />
-//     </>
-//   );
-// };
-
-// export default App;
